25889511e1ea6950e3c693ddc3732d513d048b3d
András Szabácsik <packman@freemail.hu>
2016-10-14 20:35:47 +0200

    Session Start

--- index.php
+++ index.php
@@ -4,14 +4,25 @@ class session
 {
     public function __construct ()
     {
-    	session_start ();
+        if ( session_status () == PHP_SESSION_NONE )
+        {
+            session_set_cookie_params ( 3600 );
+            session_start ();
+            $_SESSION = array ();
+        }
     }
 
     public function __destruct ()
     {
+        session_commit ();
+    }
+
+    public function id ()
+    {
+        return session_id ();
     }
 }
 
 $session = new session ();
 
-?>
+print $session -> id ();
