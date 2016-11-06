35f508de1d3a8a9a584a6b0cb71fac18bec2e0dc
András Szabácsik <packman@freemail.hu>
2016-11-06 22:09:11 +0100

    Daily Commit

--- session.class.php
+++ session.class.php
@@ -14,12 +14,13 @@ class session
 
     private function start ()
     {
+        //print getcwd();
         $_SESSION [ 'id' ] = session_id ();
         $_SESSION [ 'start_timestamp' ] = microtime ( true );
         $_SESSION [ 'expiration_timestamp' ] = floatval ( $_SESSION [ 'start_timestamp' ] ) + floatval ( ini_get ( 'session.cookie_lifetime' ) );
         $now = DateTime::createFromFormat ( 'U.u', microtime ( true ) );
         $debug = var_export ( $now, true );
-        file_put_contents('session_dump.txt',$debug,FILE_APPEND);
+        #file_put_contents('session_dump.txt',$debug,FILE_APPEND);
         $expiration = DateTime::createFromFormat ( 'U.u', floatval ( microtime ( true ) ) + floatval ( ini_get ( 'session.cookie_lifetime' ) ) );
         $_SESSION [ 'start_datetime' ] = $now -> format ( "Y-m-d H:i:s.u" );
         $_SESSION [ 'expiration_datetime' ] = $expiration -> format ( "Y-m-d H:i:s.u" );
